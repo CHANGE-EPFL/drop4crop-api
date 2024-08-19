00bc1a2851b8cdb56cb7fb1763e8beb20653420d
Evan Thomas <evan.thomas@epfl.ch>
2024-08-19 14:06:38 +0200

    Attempt to close db connection after use

--- app/db.py
+++ app/db.py
@@ -22,3 +22,4 @@ async_session = sessionmaker(
 async def get_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session() as session:
         yield session
+        await session.close()
